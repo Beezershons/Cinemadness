d0d33cadf5263496f18c2c1310253e44a350ce78
Alex Place <alexander.n.place@gmail.com>
2023-12-11 11:31:05 -0500

    minor edits to the CSS and HTML for appearance purposes. Also added an image in the footer.

--- assets/js/scrpit.js
+++ assets/js/scrpit.js
@@ -1,5 +1,7 @@
 // script.js
 
+// It looks like we are missing the link to the jQuery library.
+
 $(document).ready(function () {
     const apiKey = '2f7924a1e90e50c355edf3798e0bf400';
   
@@ -27,6 +29,7 @@ $(document).ready(function () {
           $('#movieYear').text(`Year: ${movie.release_date.substring(0, 4)}`);
           $('#movieCover').html(`<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="Movie Poster">`);
         },
+        // Should this error be a modal so the user knows there is an error? Otherwise nothing will happen on the website and the user will assume the site doesn't work.
         error: function (error) {
           console.error('Error fetching random movie:', error);
         },
