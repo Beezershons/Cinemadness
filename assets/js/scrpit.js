1278f9f74c24656c863b4b8e6ed62bfcb7068f5f
Jon-Luke <jonlukejenkins2@gmail.com>
2023-12-14 20:43:54 -0600

    new api

--- assets/js/scrpit.js
+++ assets/js/scrpit.js
@@ -4,8 +4,6 @@ $(document).ready(function () {
     // Set up event listener for changes in streaming service and genre
     $('#streamingService, #genreChoices').change(function () {
         getMovieProviders();
-       
-
     });
 
     // Set up event listener for the "Randomize" button
@@ -18,7 +16,7 @@ $(document).ready(function () {
             url: 'https://api.themoviedb.org/3/watch/providers/movie',
             method: 'GET',
             headers: {
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjc5MjRhMWU5MGU1MGMzNTVlZGYzNzk4ZTBiZjQwMCIsInN1YiI6IjY1NzI3NGRiMjExY2U1MDExYmZlY2YyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Q4XaeeNH7quaqUxYCtRma0vhqh6js0QBiN4MQxfZY2s',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjc5MjRhMWU5MGU1MGMzNTVlZGYzNzk4ZTBiZjQwMCIsInN1YiI6IjY1NzI3NGRiMjExY2U1MDExYmFjY2FkZjYyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Q4XaeeNH7quaqUxYCtRma0vhqh6js0QBiN4MQxfZY2s',
                 'Accept': 'application/json',
             },
             data: {
@@ -37,6 +35,7 @@ $(document).ready(function () {
             },
         });
     }
+
     async function fetchStreamingServiceIds() {
         const streamingServiceMap = {
             netflix: null,
@@ -45,7 +44,6 @@ $(document).ready(function () {
             paramount_plus: null,
             disney_plus: null,
             hbo_max: null,
-
             // Add more streaming services as needed
         };
 
@@ -56,28 +54,31 @@ $(document).ready(function () {
                     method: 'GET',
                     data: {
                         api_key: apiKey,
-                        region: 'US', 
+                        region: 'US',
                     },
                     dataType: 'json',
                 });
 
                 console.log('API Response:', response);
+
                 // Find the streaming service ID from the API response
                 const serviceData = response.results.find(item => item.provider_name.toLowerCase() === service);
+
                 if (serviceData) {
                     streamingServiceMap[service] = serviceData.provider_id.toString();
                 }
             }
         }
+
         console.log('API Request URL:', 'https://api.themoviedb.org/3/watch/providers', { api_key: apiKey, region: 'US' });
 
         // Now, streamingServiceMap will be populated with actual TMDB streaming service IDs
         console.log('Updated Streaming Service IDs:', streamingServiceMap);
 
         // Call getRandomMovie() to fetch a random movie based on the selected streaming service and genre
-        const genreId = ''
-        const streamingServiceId = ''
-        const genre = ''
+        const genreId = '';
+        const streamingServiceId = '';
+        const genre = '';
         console.log('streamingServiceId', streamingServiceId);
         getRandomMovie();
         generateRandomMovieForPlaceholder(1, genreId, streamingServiceId, genre);
@@ -90,12 +91,12 @@ $(document).ready(function () {
         const streamingService = $('#streamingService').val();
         const genre = $('#genreChoices').val();
         const providerName = $('#streamingService').val();
-        
+
         // Map streaming services to TMDB API endpoints
         const streamingServiceMap = {
-            netflix: '8', // Replace '8' with the actual TMDB streaming service id for Netflix
-            amazon_prime: '119', // Replace '119' with the actual TMDB streaming service id for Amazon Prime
-            // Add more streaming services as needed
+            netflix: '8', 
+            amazon_prime: '119',
+        
         };
 
         // Get the TMDB streaming service id for the selected streaming service
@@ -112,6 +113,15 @@ $(document).ready(function () {
             success: function (data) {
                 const randomIndex = Math.floor(Math.random() * data.results.length);
                 const movie = data.results[randomIndex];
+                console.log('Random Movie:', movie); 
+
+                const imdbId = movie.imdb_id;
+                console.log('IMDb ID:', imdbId); 
+                const movieRating = getMovieDetailsFromOMDB(imdbId);
+
+
+                            // Debugging: Log the retrieved movie details
+            console.log('Random Movie Details:', movie);
 
                 // Update the HTML with movie details
                 $('#movieTitle').text(`Title: ${movie.title}`);
@@ -119,6 +129,11 @@ $(document).ready(function () {
                 $('#movieYear').text(`Year: ${movie.release_date.substring(0, 4)}`);
                 $('#movieCover').html(`<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" width= "250" height= "375" alt="Movie Poster">`);
                 $(`#movieService`).text(`Streaming Service: ${providerName}`);
+
+                            // Debugging: Log the IMDb ID
+            console.log('IMDb ID:', movie.imdb_id);
+
+                getMovieDetailsFromOMDB(movie.imdb_id);
             },
             error: function (error) {
                 console.error('Error fetching random movie:', error);
@@ -126,6 +141,33 @@ $(document).ready(function () {
         });
     }
 
+    function getMovieDetailsFromOMDB(imdbId) {
+        const omdbApiKey = '55929535'; 
+        const omdbApiUrl = `https://www.omdbapi.com/?apikey=${omdbApiKey}&i=${imdbId}`;
+    
+        $.ajax({
+            url: omdbApiUrl,
+            method: 'GET',
+            dataType: 'json',
+            success: function (omdbData) {
+                // Extract ratings from the OMDB API response and update your UI
+                const imdbRating = omdbData.imdbRating;
+                const rottenTomatoesRating = omdbData.Ratings.find(rating => rating.Source === 'Rotten Tomatoes');
+    
+                // Now you can use these ratings to update your UI or perform other actions
+                console.log('IMDb Rating:', imdbRating);
+                console.log('Rotten Tomatoes Rating:', rottenTomatoesRating ? rottenTomatoesRating.Value : 'N/A');
+    
+                // Update UI with ratings
+                $('#imdbRating').text(`IMDb Rating: ${imdbRating}`);
+                $('#rottenTomatoesRating').text(`Rotten Tomatoes Rating: ${rottenTomatoesRating ? rottenTomatoesRating.Value : 'N/A'}`);
+            },
+            error: function (error) {
+                console.error('Error fetching movie details from OMDB:', error);
+            },
+        });
+    }
+    
     // Helper function to map genre names to TMDB genre ids
     function getGenreId(genre) {
         const genreMap = {
@@ -142,6 +184,7 @@ $(document).ready(function () {
         return genreMap[genre] || '';
     }
 });
+
 // Function to generate a random movie and update the UI for the other random movie placeholders
 function generateRandomMovieForPlaceholder(placeholderNumber, genreId, streamingServiceId, genre) {
     const providerName = $('#streamingService').val();
@@ -173,12 +216,13 @@ function generateRandomMovieForPlaceholder(placeholderNumber, genreId, streaming
         }
     });
 }
-    $(document).ready(function () {
-        $('button').click(function () {
-            // Get random movies for all four placeholders
-            generateRandomMovieForPlaceholder(1, '27', '8', 'Horror');
-            generateRandomMovieForPlaceholder(2, '35', '119', 'Comedy');
-            generateRandomMovieForPlaceholder(3, '18', '8', 'Drama');
-            generateRandomMovieForPlaceholder(4, '10749', '119', 'Romance'); 
-        });
+
+$(document).ready(function () {
+    $('button').click(function () {
+        // Get random movies for all four placeholders
+        generateRandomMovieForPlaceholder(1, '27', '8', 'Horror');
+        generateRandomMovieForPlaceholder(2, '35', '119', 'Comedy');
+        generateRandomMovieForPlaceholder(3, '18', '8', 'Drama');
+        generateRandomMovieForPlaceholder(4, '10749', '119', 'Romance');
     });
+});
